7a8e0d4c54a3a6b5cf21edf6c6fa10fdd0df39e7
emirhanlatif <77436095+emirhanlatif@users.noreply.github.com>
2023-03-27 20:10:52 +0300

    Ilk_commit

---
+++ 01_Image_Basics_with_OpenCV/01_opening_image_with_openCV.py
@@ -0,0 +1,9 @@
+﻿import cv2
+
+img=cv2.imread("C:\\Users\\Emirhan\\Desktop\\DeepLearning\\Computer_Vision_with_OpenCV_and_DeepLearing\\My_CV_Course\\DATA\\00-puppy.jpg")
+new_img=cv2.resize(img,(640,480))
+
+cv2.imshow('puppy',new_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
